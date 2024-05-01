30da5de3f0eb771402b678f0b44fde166201c072
alpergel <aalisveris04@gmail.com>
2024-05-01 00:13:53 -0700

    pls?

--- app.py
+++ app.py
@@ -8,15 +8,63 @@ st.set_page_config(layout="wide", page_title="GDP Visualization", page_icon=":ch
 def load_data():
     # Simulate loading simple data
     # Assume data format: latitude, longitude, GDP, community_name
-    data = pd.DataFrame({
+    ipcData = pd.DataFrame({
         'lat': [33.36607, 33.370236, 33.369961, 33.354334],  # Check these coordinates
         'lon': [-111.96315, -111.971805, -111.953024, -111.95699],  # Check these coordinates
         'ipc': [26814, 55927, 40759, 37540],  # Simulated GDP values
         'community_name': ['Community A', 'Community B', 'Community C', 'Community D']
     })
-    return data
+    arcData = pd.DataFrame({
+        'lng_h': [-109.228377],
+        'lng_w': [-111.96315],
+        "lat_h": [29.529887],
+        "lat_w": [33.36607],
+    })
+    return ipcData, arcData
+def render_migration_map(data):
+    tooltip = {
+        "html": "<b>Location:</b> {community_name}<br><b>",
+        "style": {
+            "backgroundColor": "steelblue",
+            "color": "white"
+        }
+    }
+    GREEN_RGB = [0, 255, 0, 40]
+    RED_RGB = [240, 100, 0, 40]
+    arc_layer = pdk.Layer(
+        "ArcLayer",
+        data=data,
+        get_width="S000 * 2",
+        get_source_position=["lng_h", "lat_h"],
+        get_target_position=["lng_w", "lat_w"],
+        get_tilt=15,
+        get_source_color=RED_RGB,
+        get_target_color=GREEN_RGB,
+        pickable=True,
+        auto_highlight=True,
+    ) 
+    meanLat = (data['lat_h'].mean()+data['lat_w'].mean())//2
+    meanLon = (data['lng_h'].mean()+data['lng_w'].mean())//2
+    view_state = pdk.ViewState(
+        latitude=meanLat,
+        longitude=meanLon,
+        bearing=45,
+        pitch=50,
+        zoom=8,
+    )
 
-def render_map(data):
+
+    # Create the deck
+    st.pydeck_chart(pdk.Deck(
+        map_style="mapbox://styles/mapbox/dark-v9",
+        layers=[arc_layer],
+        initial_view_state=view_state,
+        tooltip=tooltip,
+        
+        
+    ))
+
+def render_ipc_map(data):
     tooltip = {
         "html": "<b>Census Tract:</b> {community_name}<br><b>Income Per Capita:</b> {ipc}",
         "style": {
@@ -57,16 +105,17 @@ def render_map(data):
         
     ))
 st.title("Guadalupe: A Rooted City of Hope and Migration")
-data = load_data()
+ipcData, arcData = load_data()
+render_migration_map(arcData)
 row1_1, row1_2, row1_3 = st.columns(3)
 with row1_1:
     st.title("Local Income Per Capita Comparison")
-    render_map(data)
+    render_ipc_map(ipcData)
 with row1_2:
     st.title("Local Income Per Capita Comparison")
-    render_map(data)
+    render_ipc_map(ipcData)
 with row1_3:
     st.title("Local Income Per Capita Comparison")
-    render_map(data)
+    render_ipc_map(ipcData)
 
 
