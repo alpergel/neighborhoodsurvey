1c78bdc38159b3c5d59ec5ffbcea2f4df80f42b4
alpergel <aalisveris04@gmail.com>
2024-04-30 23:40:50 -0700

    pls?

--- app.py
+++ app.py
@@ -32,7 +32,7 @@ def render_map(data):
         get_elevation="ipc",
         auto_highlight=True,
         get_fill_color=[255,0, 0],  
-        elevation_scale=0.10,  # Adjusted for visibility
+        elevation_scale=0.05,  # Adjusted for visibility
         radius=50,  # Visible radius
         extruded=True,
         pickable=True,
@@ -57,7 +57,7 @@ def render_map(data):
         
     ))
 
-st.title("Interactive GDP Visualization")
+st.title("Local Income Per Capita Comparison")
 # Main
 data = load_data()
 render_map(data)
